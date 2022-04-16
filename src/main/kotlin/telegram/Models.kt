c6bd37bb7f435faf0f80c98f493f9f4875fc7dc6
Сергей Романов <69107006+oQaris@users.noreply.github.com>
2022-04-16 12:35:36 +0600

    Новый типобезопасный строитель таблиц
    Писал с телефона, без форматирования

--- src/main/kotlin/telegram/Models.kt
+++ src/main/kotlin/telegram/Models.kt
@@ -35,23 +35,63 @@ data class TextItem(val text: String, val type: String? = null)
 
 // Для вывода
 
-class Table(private val padding: Int = 1, private val columns: MutableList<List<String>> = mutableListOf()) :
+class Table private(private val padding: Int = 1, private val columns: MutableList<List<String>> = mutableListOf()) :
     MutableList<List<String>> by columns {
 
-    fun print() {
-        if (columns.isEmpty() || columns.all { it.isEmpty() }) return
+    fun print() = println(formatedRows().joinToString("\n"))
+
+fun formatedRows() = buildList{
+if (columns.isEmpty() || columns.all { it.isEmpty() }) return
         val widths = columns.map { col -> col.maxOf { it.length } }
         val height = columns.maxOf { it.size }
         for (row in 0 until height) {
+var rowStr = "" 
             for (col in columns.indices) {
                 val content = if (row < columns[col].size) columns[col][row] else ""
-                print(content.padEnd(widths[col] + padding))
+                rowStr+=content.padEnd(widths[col] + padding)
             }
-            println()
-        }
-    }
+            add(rowStr) 
+        } 
+} 
 
     fun addColumn(header: String, data: Iterable<Any>) {
         add(listOf(header) + data.map { it.toString() })
     }
+
+companion object {
+
+class Builder(val padding: Int){
+private val columns = mutableListOf<List<String>>() 
+private abstract class Appender{
+val columns: MutableList<List<String>>
+fun add(vararg data: Any) 
 }
+private class RowsAppender{
+fun add(vararg rowData: Any) {
+if(columns.isEmpty())
+columns.addAll(rowData.map{listOf(it.toString())})
+else columns.zip(rowData).forEach{col, el -> col.add(el.toString())}
+}
+}
+private class ColumnsAppender{
+fun add(vararg columnData: Any) {
+columns.add(columnData.map { it.toString() })  
+} 
+}
+} 
+fun withRows(append: RowAppender.() -> Unit):Table{
+RowAppender(columns).append()
+return Table(padding, columns) 
+} 
+
+fun withColumns(append: ColumnAppender.() -> Unit):Table{
+ColumnAppender(columns).append()
+return Table(padding, columns) 
+}
+
+private fun buildTable(appender:Appender): Table{
+//ToDo
+} 
+}
+}
+} 
