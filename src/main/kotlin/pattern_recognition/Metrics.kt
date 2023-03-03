7300d59cb14bf0b601c332d068a309c894f97e68
oqaris <oqaris@deeplay.io>
2023-03-03 10:54:17 +0600

    Добавлены метрики, демонстрация точности

--- src/main/kotlin/pattern_recognition/Metrics.kt
+++ src/main/kotlin/pattern_recognition/Metrics.kt
@@ -1,5 +1,6 @@
 package pattern_recognition
 
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -12,7 +13,18 @@ data class Item(val data: Data, val clazz: Clazz)
 infix fun Data.itm(that: Clazz) = Item(this, that)
 
 
+fun manhattan(v1: Data, v2: Data) =
+    v1.zip(v2).sumOf {
+        abs(it.first - it.second)
+    }.toDouble()
+
 fun euclidean(v1: Data, v2: Data) =
     v1.zip(v2).sumOf {
         (it.first - it.second).toDouble().pow(2)
     }.let { sqrt(it) }
+
+fun cosine(v1: Data, v2: Data) = v1.zip(v2).run {
+    sumOf { it.first * it.second } /
+            (sqrt(sumOf { it.first.toDouble().pow(2) })
+                    * sqrt(sumOf { it.second.toDouble().pow(2) }))
+}
---
+++ src/main/kotlin/pattern_recognition/Utils.kt
@@ -0,0 +1,12 @@
+package pattern_recognition
+
+import kotlin.random.Random
+
+fun <T> Iterable<T>.sortedCounter(grouping: (T) -> T = { it }) =
+    groupingBy(grouping).eachCount().toList().sortedByDescending { it.second }
+
+fun Data.noise(p: Float) = map {
+    if (Random.nextFloat() < p)
+        (it + 1) % 2
+    else it
+}
