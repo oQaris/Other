3d42dc1ba67d1b0b5638917b879aa29778199e30
Sergey Romanov <romanovomsk2014@gmail.com>
2023-08-17 14:11:15 +0600

    Доработка BotVersionUpdater для работы с последним коммитом, а не напрямую с изменёнными файлами, ради повышения безопасности.

--- PropertiesCollector/src/main/kotlin/bot_version_updater/BotVersionUpdater.kt
+++ PropertiesCollector/src/main/kotlin/bot_version_updater/BotVersionUpdater.kt
@@ -1,40 +1,86 @@
 package bot_version_updater
 
 import java.io.File
-import kotlin.system.exitProcess
 
 /**
- * test
- * <h2>368<h2>
- * 23452345
+ * test description
+ * <h2>369<h2>
+ * [+] test message
  */
 class BotVersionUpdater {
     private val gitModifiedFilesPattern = "\\s*M\\s+(.+)".toRegex()
     private val gitDiffLinesPattern = "[+-]\\s+(.+)".toRegex()
     private val botCommitNotesPattern = "\\s*\\*/".toRegex()
     private val botVersionPattern = "public static final int \\w*BOT\\w*_VERSION = (\\d+);".toRegex()
-    private val test = "public static final int BOT_VERSION = 368;"
+    private val test = "public static final int BOT_VERSION = 369;"
 
     fun update(message: String) {
-        val botClasses = getNecessaryFiles()
+        val botClasses = getFilesWithUnmodifiedVersions()
         if (botClasses.isEmpty()) {
-            println("No bot classes")
+            println("No changed bot classes")
             return
         }
-        println("Bots: " + botClasses.joinToString(", ") { it.name })
-        println("New versions: " +
-                botClasses.joinToString(", ") {
-                    incrementBotVersion(it, message)
+        val commitNote = message.ifBlank { extractCommitNote() }
+        println("Commit note: $commitNote")
+
+        val resultBotToVersion = botClasses.map { it.name }.zip(
+            botClasses.map {
+                try {
+                    incrementBotVersion(it, commitNote)
+                } catch (e: Exception) {
+                    "fail!"
                 }
+            }
         )
-        println("Commit note: $message")
+        println("New versions:\n" + resultBotToVersion
+            .joinToString("\n") {
+                it.first + " -> " + it.second
+            }
+        )
     }
 
-    private fun getNecessaryFiles(): List<File> {
+    private fun extractCommitNote(): String {
+        val gitLogProcess = ProcessBuilder("git", "log", "-1", "--pretty=format:%s").start()
+        val origMsg = gitLogProcess.inputStream.reader().readText()
+        return try {
+            origMsg.split("\n").first()
+                .split("-").let {
+                    it[0].trim().take(3) + " " + it[1].trim()
+                }
+        } catch (e: Exception) {
+            origMsg // Если сообщение коммита не соответствует формату
+        }
+    }
+
+    /**
+     * Заменяет текст в файле [baseClass], увеличивая значение переменной версии на 1
+     * и добавляя в документацию заметку к версии [message].
+     * @return стоку со значением новой версии
+     */
+    private fun incrementBotVersion(baseClass: File, message: String): String {
+        val textCode = StringBuilder(baseClass.bufferedReader().readText())
+
+        val versionMatch = botVersionPattern.find(textCode)!!.groups[1]!!
+        val newVersion = (versionMatch.value.toInt() + 1).toString()
+        textCode.replace(versionMatch.range.first, versionMatch.range.last + 1, newVersion)
+
+        val commentMatch = botCommitNotesPattern.find(textCode)!!
+        textCode.insert(
+            commentMatch.range.first,
+            "\n * <h2>$newVersion<h2>\n * ${message.replace("\n", "\n * ")}"
+        )
+
+        baseClass.bufferedWriter().use { bw ->
+            bw.write(textCode.toString())
+            bw.flush()
+        }
+        return newVersion
+    }
+
+    private fun getFilesWithUnmodifiedVersions(): List<File> {
         return preparedBotBaseClasses().filter { file ->
-            val gitDiffProcess = ProcessBuilder("git", "diff", file.absolutePath).start()
+            val gitDiffProcess = ProcessBuilder("git", "show", "HEAD", "--", file.absolutePath).start()
             val output = gitDiffProcess.inputStream.reader().readText()
-            println(output)
             gitDiffLinesPattern.findAll(output).all { diffLine ->
                 botVersionPattern.find(diffLine.groups[1]!!.value) == null
             }
@@ -42,22 +88,21 @@ class BotVersionUpdater {
     }
 
     private fun preparedBotBaseClasses(): List<File> {
-        return getModifiedFiles().filter {
+        return getModifiedFilesLastCommit().filter {
             it.name.contains("Bot")
         }.map { file ->
             searchBaseClassIfNeed(file)
         }
     }
 
-    private fun getModifiedFiles(): List<File> {
+    private fun getModifiedFilesLastCommit(): List<File> {
         val allRepos = listOf(File(".")) + (File("submodules").listFiles()?.toList() ?: listOf<File>())
         return buildList {
             allRepos.forEach { dir ->
-                val gitStatusProcess = ProcessBuilder("git", "status", "-s")
+                val gitStatusProcess = ProcessBuilder("git", "diff", "--name-status", "HEAD~1", "HEAD")
                     .directory(dir)
                     .start()
                 val output = gitStatusProcess.inputStream.reader().readText()
-                println(output)
                 addAll(gitModifiedFilesPattern.findAll(output).map {
                     File(it.groups[1]!!.value)
                 }.toList())
@@ -74,46 +119,16 @@ class BotVersionUpdater {
             }.firstOrNull() ?: botClass
         }
     }
-
-    private fun incrementBotVersion(baseClass: File, message: String): String {
-        try {
-            val text = StringBuilder(baseClass.bufferedReader().readText())
-
-            val versionMatch = botVersionPattern.find(text)!!.groups[1]!!
-            val newVersion = (versionMatch.value.toInt() + 1).toString()
-            text.replace(versionMatch.range.first, versionMatch.range.last + 1, newVersion)
-
-            val commentMatch = botCommitNotesPattern.find(text)!!
-            text.insert(
-                commentMatch.range.first,
-                "\n * <h2>$newVersion<h2>\n * ${message.replace("\n", "\n * ")}"
-            )
-
-            baseClass.bufferedWriter().use { bw ->
-                bw.write(text.toString())
-                bw.flush()
-            }
-            return newVersion
-        } catch (e: Exception) {
-            return "fail"
-        }
-    }
 }
 
+/**
+ * args[0] - единственная строка с примечанием к версии. Если содержит пробелы, то необходимо взять в кавычки.
+ * Может быть пустой, тогда в качестве примечания берётся информация из сообщения к последнему коммиту.
+ */
 fun main(args: Array<String>) {
     try {
-        val origMsg =
-            if (args.isEmpty()) File(".git\\COMMIT_EDITMSG").readText().trim()
-            else args.single()
-        val message = origMsg.split("\n")
-            .first().split("-").let {
-                it[0].trim().take(3) + " " + it[1].trim()
-            }
-        BotVersionUpdater().update(message)
-        Thread.sleep(1000)
+        BotVersionUpdater().update(if (args.isEmpty()) "" else args.single())
     } catch (e: Exception) {
         e.printStackTrace()
-        exitProcess(1)
     }
-    exitProcess(0)
 }
